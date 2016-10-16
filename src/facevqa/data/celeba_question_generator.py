a14d795adc5293d743b5c35348243c73aede58bd
Sina Samangooei <sinjax@gmail.com>
2016-10-16 15:05:29 +0100

    some more changeS

--- src/facevqa/data/celeba_question_generator.py
+++ src/facevqa/data/celeba_question_generator.py
@@ -1,6 +1,7 @@
 import json
 
 from pymantic import sparql
+from rdflib import Graph
 
 
 def _from_dict(target, d):
@@ -63,18 +64,34 @@ class Question(object):
 
 
 
+class BalzeGraphConnector(object):
+    def __init__(self,host="192.168.0.3:9999", namespace="kb"):
+        sparql_server = "http://%(host)s/blazegraph/namespace/%(namespace)s/sparql"%locals()
+        self.server = sparql.SPARQLServer(sparql_server)
+        super().__init__()
+
+    def query(self, q):
+        return self.server.query(q)
+
+class RDFLibConnector(object):
+    def __init__(self, graph):
+        self.graph = Graph()
+        self.graph.parse("/Users/ss/Data/celeba/list_attr_celeba.rdf.xml")
+
+    def query(self, q):
+        return self.graph.query(q)
+
 
 
 class QuestionGenerator(object):
-    def __init__(self, question_config, host="192.168.0.3:9999", namespace="kb"):
-        # http://192.168.0.3:9999/blazegraph/namespace/kb/sparql
-        sparql_server = "http://%(host)s/blazegraph/namespace/%(namespace)s/sparql"%locals()
-        self.server = sparql.SPARQLServer(sparql_server)
+    def __init__(self, question_config, sparql_connector):
         self.questions = {}
+        self.server = sparql_connector
         with open(question_config) as question_f:
             for question_json in json.load(question_f):
                 q = Question.read(question_json)
                 self.questions[q.name] = q
+
     def list_people(self):
         return self.server.query("select ?person {?person a <http://schema.org/Person>}")
 
@@ -87,7 +104,6 @@ class QuestionGenerator(object):
             for question_name, question in self.questions.items():
                 if not question.query_is_valid(): continue
                 query_person = question.query_person(person)
-                print(query_person)
                 query_result = self.server.query(query_person)
                 ret["question_answers"] += [question.construct_question_answer(query_result)]
             yield ret
@@ -98,15 +114,18 @@ def _as_uri(param):
 
 
 if __name__ == '__main__':
-    gen = QuestionGenerator("templates/questions.json")
+    connector = BalzeGraphConnector()
+    gen = QuestionGenerator("templates/questions.json", connector)
     people = gen.list_people()
     one_person = _as_uri(people["results"]["bindings"][0]["person"])
-    for person_question_answers in gen.generate_questions(one_person):
-        print("""%(person)s"""%person_question_answers)
-        for question_answers in person_question_answers["question_answers"]:
-            if len(question_answers) == 0: continue
-            print("""   %(name)s"""%question_answers[0])
-            for question_answer in question_answers:
-                print("""      %(question)s?   %(answer)s"""%question_answer)
+    for person in people["results"]["bindings"][:100]:
+        person_uri = _as_uri(person["person"])
+        for person_question_answers in gen.generate_questions(person_uri):
+            print("""%(person)s"""%person_question_answers)
+            for question_answers in person_question_answers["question_answers"]:
+                if len(question_answers) == 0: continue
+                print("""   %(name)s"""%question_answers[0])
+                for question_answer in question_answers:
+                    print("""      %(question)s?   %(answer)s"""%question_answer)
 
 
